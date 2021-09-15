a3e4cc5e3efbc438365676b61a5d606d7693d019
rebelate <arsilq@yahoo.com>
2021-09-15 14:19:48 +0700

    Update

--- dunst/volp.sh
+++ dunst/volp.sh
@@ -12,6 +12,7 @@ cur_sink=$([ -n "$sink" ] && echo "$sink" || echo "$def_sink")
 function get_volume {
   pactl list sinks | grep -A 15 -E "^Sink #$cur_sink\$" | grep 'Volume:' | grep -E -v 'Base Volume:' | awk -F : '{print $3; exit}' | grep -o -P '.{0,3}%' | sed 's/.$//' | tr -d ' '
 }
+echo $cur_sink
 volume=`get_volume`
 volume_step=6
 volume_limit=$((100 - $volume_step))
@@ -60,7 +61,6 @@ case $1 in
 	# Set the volume on (if it was muted)
 	amixer set Master on > /dev/null
 	# Up the volume (+ 5%)
-	echo "$volume"
 	if [ "$volume" -le "100" ] && [ "$volume" -ge "$volume_limit" ]; then
 	  pactl set-sink-volume "$cur_sink" "100%"
     elif [ "$volume" -lt $volume_limit ]; then
